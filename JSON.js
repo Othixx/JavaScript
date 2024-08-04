07d2e6ec3f9423ce990e7a3a310336c57cd11858
Othixx <othixx@qq.com>
2024-08-04 10:26:14 +0800

    update

---
+++ JSON.js
@@ -0,0 +1,12 @@
+let xiaoming = {
+    name: '小明',
+    age: 14,
+    gender: true,
+    height: 1.65,
+    grade: null,
+    'middle-school': '\"W3C\" Middle School',
+    skills: ['JavaScript', 'Java', 'Python', 'Lisp']
+};
+
+let s = JSON.stringify(xiaoming, null, '  ');
+console.log(s);
--- RegExp.js
+++ RegExp.js
@@ -1,3 +1,5 @@
 let re1 = /ABC\-001/;
 
 console.log(re1.test('ABC-0012'));
+
+console.log('a,b;; c  d'.split(/[\s\,\;]+/)); // ['a', 'b', 'c', 'd']
